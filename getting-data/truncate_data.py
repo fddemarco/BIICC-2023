3a5cd14d22237fe7c905b117b8845c5b8b241951
fddemarco <franco.demarco3400@gmail.com>
2023-05-19 13:15:41 -0300

    Feature: Se permite truncar dataset y generar un nuevo dataset con los datos truncados

--- getting-data/truncate_data.py
+++ getting-data/truncate_data.py
@@ -1,49 +1,107 @@
-import os
-from glob import glob
+import numpy as np
 
 import pandas as pd
-import pyarrow.compute as pc
 import pyarrow.dataset as ds
+import pyarrow as pa
 
 
-def truncate_data(dataset_source, year, subreddit, text_len_threshold, data_dir):
+def get_most_popular_subreddits(dataset):
     """
-        Ejemplo de uso:
-            truncate_data("RS", 2012, "TwoXChromosomes", 100000)
+    Esto funciona porque no son tantos submissions
+    y los subreddits tienen nombres relativamente cortos.
+    (Entran en memoria).
+
+    Devuelve los 10 mil subreddits con más posts, ordenados de menor a mayor.
+
     """
-    data_files_pattern = os.path.join(data_dir, f"{dataset_source}_{year}-[0-9]*_[0-9]*.parquet")
-    data_files = glob(data_files_pattern)
+    subreddits = (
+        dataset
+        .to_table(columns=["subreddit"])
+        .column("subreddit")
+        .to_pandas()
+        .value_counts()
+        .sort_values(ascending=True)[-10000:]
+    )
+    return subreddits
 
-    dataset = ds.dataset(data_files)
-    dataset = dataset.filter(pc.field("subreddit") == subreddit)
-    sorted_dataset = dataset.sort_by([("score", "descending")])
 
+def partition_threshold(subreddits):
+    return int(subreddits.sum()) // 100
+
+
+def split_subreddits(subreddits):  # Subset sum
+    splits = []
+    current_split = []
+    current_cum_count = 0
+    threshold = partition_threshold(subreddits)
+    for s, s_count in subreddits.items():
+        if threshold < current_cum_count + s_count:
+            splits.append(current_split)
+            current_cum_count = 0
+            current_split = []
+        current_cum_count += s_count
+        current_split.append(s)
+
+    if current_split:
+        splits.append(current_split)
+
+    return splits
+
+
+def write_splits(dataset, splits, base_dir):
+    for idx, ss in enumerate(splits):
+        dataset_filtered = dataset.filter(ds.field("subreddit").isin(ss))
+        write_options = ds.ParquetFileFormat().make_write_options(version="2.6")
+        ds.write_dataset(
+            dataset_filtered,
+            base_dir=base_dir,
+            basename_template=f"part-{{i}}{idx}.parquet",
+            file_options=write_options,
+            format="parquet",
+            existing_data_behavior="overwrite_or_ignore"
+        )
+
+
+def truncate_splits(splits, base_dir, text_len_threshold):
     dfs = []
-    for chunk in sorted_dataset.to_batches():
-        df = chunk.to_pandas()
-        df["text"] = df["title"] + " " + df["selftext"]
-        df["cumulative_len"] = df["text"].str.len().cumsum()
-        if text_len_threshold <= df["cumulative_len"].iloc[-1]:
-            df = df[df["cumulative_len"] <= text_len_threshold]
-            dfs.append(df)
-            break
-        else:
-            dfs.append(df)
+    for i, ss in enumerate(splits):
+        dataset = ds.dataset(f"{base_dir}/part-0{i}.parquet", format="parquet")
+        for s in ss:
+            dataset_s = dataset.filter(ds.field("subreddit") == s)
+            df_s = truncate_data(dataset_s, text_len_threshold)
+            dfs.append(df_s)
     result = pd.concat(dfs).reset_index(drop=True)
     return result
 
 
-if __name__ == "__main__":
+def truncate_data(dataset, text_len_threshold):
+    df = dataset.to_table().to_pandas()
+    df.sort_values("score", ascending=False)
+    df["text"] = df["title"] + " " + df["selftext"]
+    df["cumulative_len"] = df["text"].str.len().cumsum()
+    first_row = np.zeros(len(df), dtype=bool)
+    first_row[0] = True
+    df = df[
+        (df["cumulative_len"] <= text_len_threshold) | first_row
+        ]
+    return df
+
+
+def get_data():
     data_dir = input("Enter data dir: ")
     results_dir = input("Enter results dir: ")
-    year = 2012
-    unique_subreddits = ["democrats"]
-    subreddit_texts = []
-    subreddit_cant = []
-    for subR in unique_subreddits:
-        dfR = truncate_data("RS", year, subR, 10000, data_dir)
-        output_pathname = os.path.join(results_dir, "democrats.csv")
-        dfR.to_csv(output_pathname, index=False)
+    partitioning = ds.partitioning(
+        pa.schema([
+            ("month", pa.int16())
+        ])
+    )
+    dataset = ds.dataset(data_dir, format="parquet", partitioning=partitioning)
+    subreddits = get_most_popular_subreddits(dataset)
+    splits = split_subreddits(subreddits)
+    write_splits(dataset, splits, results_dir)
+    df = truncate_splits(splits, results_dir, 10000)
+    df.to_parquet(f"{results_dir}/results.parquet")
 
-        subreddit_texts.append(' '.join(dfR['title'] + ' ' + dfR['text']).replace('\r', '').replace('\n', ''))
-        subreddit_cant.append(dfR.shape[0])
+
+if __name__ == "__main__":
+    get_data()
