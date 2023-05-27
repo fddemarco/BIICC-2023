89b928dfd4c90b4344d6eecc1f3ca73a71848c13
fddemarco <franco.demarco3400@gmail.com>
2023-05-27 02:27:19 -0300

    Feature: aplicacion de consola para descargar y limpiar datos

---
+++ pushshift-reddit-script/app.py
@@ -0,0 +1,60 @@
+import click
+
+from dataset import Submissions
+from dataset import Comments
+
+
+@click.command()
+@click.option("-t", "--posts-type", "posts_type",
+              help="Specify the type of Reddit posts to process: s (submissions) / c (comments).",
+              type=click.Choice(['submissions', 'comments']),
+              prompt=True,
+              )
+@click.option("-y", "--year", "year",
+              help="Specify the year of the posts to process (2012-2018).",
+              type=click.IntRange(2012, 2018),
+              prompt=True,
+              )
+@click.option("-wd", "--working-dir", "working_dir",
+              help="Specify the working directory.",
+              type=click.Path(exists=True, readable=True, writable=True),
+              default='.'
+              )
+@click.option("--download",
+              help="Download specified dataset.",
+              is_flag=True,
+              show_default=True,
+              default=False
+              )
+@click.option("--clean",
+              help="Clean specified dataset.",
+              is_flag=True,
+              show_default=True,
+              default=False
+              )
+def app(posts_type: str,
+        year: str,
+        working_dir: str,
+        download: bool,
+        clean: bool
+        ) -> None:
+    """
+    Process a specific Reddit POSTS TYPE (submissions or comments) for a given YEAR.
+    \f
+    :param posts_type: Reddit posts type to process (comments/submissions)
+    :param year: Year of the posts (a value between 2012 and 2018)
+    :param working_dir: Working directory.
+    :param download: Download specified dataset.
+    :param clean: Clean downloaded dataset.
+    :return: None
+    """
+    dataset = Submissions(year, working_dir) if posts_type == 'submissions' else Comments(year, working_dir)
+
+    if download:
+        dataset.download_data()
+    if clean:
+        dataset.clean_dataset()
+
+
+if __name__ == "__main__":
+    app()
---
+++ pushshift-reddit-script/dataset.py
@@ -0,0 +1,164 @@
+import re
+
+import huggingface_hub as hf_hub
+import pathlib
+import pyarrow.dataset as ds
+import pyarrow.parquet as pq
+import pyarrow as pa
+
+
+class PushshiftRedditPostsDataset:
+    def __init__(self, year, working_directory):
+        """
+        Represents a dataset of Reddit posts for a given YEAR.
+
+        :param year: Year of the posts (a value between 2012 and 2018)
+        """
+        self.year = str(year)
+        self.working_dir = pathlib.Path(working_directory)
+
+    @property
+    def dataset_id(self):
+        raise NotImplementedError("Should be implemented in a subclass.")
+
+    @property
+    def huggingface_dataset(self):
+        raise NotImplementedError("Should be implemented in a subclass.")
+
+    @property
+    def schema(self):
+        raise NotImplementedError("Should be implemented in a subclass.")
+
+    @property
+    def text_features(self):
+        raise NotImplementedError("Should be implemented in a subclass.")
+
+    def name(self, month):
+        return f"{self.dataset_id}_{self.year}-{month}"
+
+    def download_data(self):
+        hf_hub.snapshot_download(
+            repo_id=self.repo_id,
+            repo_type="dataset",
+            allow_patterns=self.hf_filename_pattern,
+            local_dir=self.hf_data_dir,
+            cache_dir=self.cache_dir,
+            local_dir_use_symlinks=True
+        )
+
+    @property
+    def hf_filename_pattern(self):
+        return f"RS_{self.year}-[0-9]*.parquet"
+
+    @property
+    def repo_id(self):
+        return "Aoppenhiem/pushshift-reddit"
+
+    @property
+    def cache_dir(self):
+        return self.working_dir / 'cache'
+
+    def clean_dataset(self):
+        self.create_dir_structure()
+        for month in self.months:
+            self.clean_monthly_dataset(month)
+
+    def create_dir_structure(self):
+        if not self.clean_dir.exists():
+            self.clean_dir.mkdir(parents=True)
+
+    def clean_monthly_dataset(self, month):
+        dataset = ds.dataset(self.hf_pathname(month), format="parquet", schema=self.schema)
+        with pq.ParquetWriter(self.clean_pathname(month), self.schema, version="2.6") as writer:
+            for batch in dataset.to_batches(columns=self.schema.names):
+                df = self.clean_batch(batch.to_pandas())
+                table = pa.Table.from_pandas(df, schema=self.schema)
+                writer.write_table(table)
+
+    @property
+    def months(self):
+        return [str(i).zfill(2) for i in range(1, 12 + 1)]
+
+    def hf_pathname(self, month):
+        return self.hf_data_dir / f'RS_{self.year}-{month}.parquet'
+
+    def data_dir(self, base_folder):
+        return self.working_dir / base_folder / self.huggingface_dataset / self.year
+
+    @property
+    def hf_data_dir(self):
+        return self.data_dir('data')
+
+    def clean_pathname(self, month):
+        return self.clean_dir / f"{self.name(month)}_00.parquet"
+
+    @property
+    def clean_dir(self):
+        return self.data_dir('data_clean')
+
+    def clean_batch(self, df):
+        regex = re.compile(r"\[[^)]*]|(@\[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+://\S+)|^rt|http.+?")
+
+        for key in self.text_features:
+            df.loc[:, key] = (df[key].fillna('')
+                              .str.replace(regex, '', regex=True)
+                              .str.lower())
+        return df[df[self.text_features].ne('').any(axis=1)]
+
+
+class Submissions(PushshiftRedditPostsDataset):
+    @property
+    def dataset_id(self):
+        return 'RS'
+
+    @property
+    def huggingface_dataset(self):
+        return 'pushshift-reddit'
+
+    @property
+    def schema(self):
+        return pa.schema(
+            [
+                ('author', pa.string()),
+                ('created_utc', pa.string()),  # TODO: debería ser int64 segun paper de waller
+                ('id', pa.string()),
+                ('num_comments', pa.int64()),
+                ('score', pa.int64()),
+                ('selftext', pa.string()),
+                ('subreddit', pa.string()),
+                ('subreddit_id', pa.string()),
+                ('title', pa.string())
+            ])
+
+    @property
+    def text_features(self):
+        return ["selftext", "title"]
+
+
+class Comments(PushshiftRedditPostsDataset):
+    @property
+    def dataset_id(self):
+        return 'RC'
+
+    @property
+    def huggingface_dataset(self):
+        return 'pushshift-reddit-comments'
+
+    @property
+    def schema(self):
+        return pa.schema(
+            [
+                ('author', pa.string()),
+                ('body', pa.string()),
+                ('controversiality', pa.int64()),
+                ('created_utc', pa.int64()),
+                ('id', pa.string()),
+                ('link_id', pa.string()),
+                ('score', pa.int64()),
+                ('subreddit', pa.string()),
+                ('subreddit_id', pa.string())
+            ])
+
+    @property
+    def text_features(self):
+        return ["body"]
