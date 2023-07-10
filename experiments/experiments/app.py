2c4c45e9dbe199d793a0aac0c9e7e383bab7262b
fddemarco <franco.demarco3400@gmail.com>
2023-07-10 12:57:12 -0300

    feat(app): add docstrings

--- experiments/experiments/app.py
+++ experiments/experiments/app.py
@@ -1,38 +1,58 @@
-from enum import Enum
+"""
+CLI is defined in this module.
+"""
 
-import typer
-from typing_extensions import Annotated
+from enum import Enum
 from pathlib import Path
+
+from typing_extensions import Annotated
+import typer
+
 
 from experiments.fasttext_experiment import FasttextExperiment
 from experiments.posts_type import Submissions, Comments
 
 
 class Options(str, Enum):
+    """
+    Options class type for CLI argument
+    """
     def __str__(self):
         return self.value
 
 
 class Types(Options):
-    submissions = "submissions"
-    comments = "comments"
+    """
+    Reddit Posts type
+    """
+    SUBMISSIONS = "submissions"
+    COMMENTS = "comments"
 
 
 class Dataset(Options):
-    original = "original"
-    truncated = "truncated"
+    """
+    Posts Dataset used
+    """
+    ORIGINAL = "original"
+    TRUNCATED = "truncated"
 
 
 class OutputDir(Options):
-    results = "results"
-    pretrained = "pretrained"
+    """
+    Output directory
+    """
+    RESULTS = "results"
+    PRETRAINED = "pretrained"
 
 
 class Command(Options):
-    texts = 'texts'
-    truncate = 'truncate'
-    embeddings = 'embeddings'
-    compare = 'compare'
+    """
+    App command
+    """
+    TEXTS = 'texts'
+    TRUNCATE = 'truncate'
+    EMBEDDINGS = 'embeddings'
+    COMPARE = 'compare'
 
 
 app = typer.Typer()
@@ -44,7 +64,8 @@ def main(working_dir: Annotated[Path,
          command: Annotated[Command,
          typer.Argument(help='Command to process.')],
          posts_type: Annotated[Types,
-         typer.Argument(help='Reddit posts type to process (comments/submissions)')] = Types.submissions,
+         typer.Argument(
+             help='Reddit posts type to process (comments/submissions)')] = Types.SUBMISSIONS,
          _from: Annotated[int,
          typer.Argument(help='Start year of the posts (a value between 2012 and 2018)',
                         min=2012,
@@ -54,9 +75,9 @@ def main(working_dir: Annotated[Path,
                         min=2012,
                         max=2018)] = 2018,
          results_dir: Annotated[OutputDir,
-         typer.Argument(help='Results folder name.')] = OutputDir.results,
+         typer.Argument(help='Results folder name.')] = OutputDir.RESULTS,
          dataset: Annotated[Dataset,
-         typer.Argument(help='Dataset folder name.')] = Dataset.original,
+         typer.Argument(help='Dataset folder name.')] = Dataset.ORIGINAL,
          ):
     """
     Process a specific Reddit POSTS TYPE (submissions or comments) for a give range of YEARs.
@@ -74,13 +95,13 @@ def main(working_dir: Annotated[Path,
             post_type,
             dataset
         )
-        if command == 'texts':
+        if command == Command.TEXTS:
             experiment.generate_texts()
-        elif command == 'truncate':
+        elif command == Command.TRUNCATE:
             experiment.generate_truncated_texts()
-        elif command == 'embeddings':
+        elif command == Command.EMBEDDINGS:
             experiment.save_embeddings_to_csv()
-        elif command == 'compare':
+        elif command == Command.COMPARE:
             experiment.compare_rankings()
         else:
             raise ValueError("Invalid command.")
