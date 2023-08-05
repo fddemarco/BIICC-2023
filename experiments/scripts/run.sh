d530c09702343d149e67e26e0f8d6b155a9f06b5
fddemarco <franco.demarco3400@gmail.com>
2023-08-05 19:53:12 -0300

    clean: remove old scripts

--- experiments/scripts/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-args="-t submissions -wd /media/franco/TOSHIBA/pushshift/data"
-for year in {2012..2018}
-do    
-    python ../src/experiments/app.py "$args" -y "$year" -e embeddings -o results
-    python ../src/experiments/app.py "$args" -y "$year" -e compare -o results
-done
-
