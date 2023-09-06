a980e18a0d4bf119680ce3ad5068841906841d15
fddemarco <franco.demarco3400@gmail.com>
2023-09-06 10:29:20 -0300

    refactor: add new method for seed augmentation

--- experiments/experiments/dimension_generator.py
+++ experiments/experiments/dimension_generator.py
@@ -32,12 +32,14 @@ class DimensionGenerator:
     """A class to generate d-ness scores from seed pairs."""
 
     def __init__(self, vectors:pd.DataFrame, nn_n: int = 10, k: int = 10):
-        """_summary_
+        """Initialize a d-ness score generator with input data.
 
         Args:
             vectors (pd.DataFrame): Input data.
-            nn_n (int, optional): Number of pairs to generate per community. Defaults to 10.
-            k (int, optional): Number of directions used to create the dimension. Defaults to 10.
+            nn_n (int, optional): Nearest Neighbours Number of pairs to generate per community. 
+            Defaults to 10.
+            k (int, optional): Number of directions used to create the dimension. 
+            Defaults to 10.
         """        
         self.vectors = pd.DataFrame(
             normalize(vectors, norm="l2", axis=1), index=vectors.index
@@ -46,8 +48,7 @@ class DimensionGenerator:
         self.k = k
         self.pairs_cache = None
 
-    @property
-    def nearest_neighbours_pairs_diff(self) -> pd.DataFrame:
+    def nearest_neighbours_directions(self) -> pd.DataFrame:
         """This is based on the aforementioned idea that we are looking for
         pairs of communities that are very similar, but differ only in the target concept.
 
@@ -55,25 +56,23 @@ class DimensionGenerator:
             pd.DataFrame: the set of all pairs of communities (c1, c2) such that c1 != c2
             and c2 is one of the nn_n nearest neighbours to c1. (nn_n = 10 by default)
         """
-        if self.pairs_cache is None:
-            comm_names = list(self.vectors.index)
-            matrix = similarity_matrix(self.vectors)
+        comm_names = list(self.vectors.index)
+        matrix = similarity_matrix(self.vectors)
 
-            kth_largest_values = np.partition(matrix, -self.nn_n, axis=1)[:, -self.nn_n]
-            indices_to_calc = np.where(matrix >= kth_largest_values[:, np.newaxis])
+        kth_largest_values = np.partition(matrix, -self.nn_n, axis=1)[:, -self.nn_n]
+        indices_to_calc = np.where(matrix >= kth_largest_values[:, np.newaxis])
 
-            pair_names = [
-                (comm_names[c1], comm_names[c2]) for c1, c2 in zip(*indices_to_calc)
-            ]
-            pairs_difference = [
-                self.vectors.iloc[c2] - self.vectors.iloc[c1]
-                for c1, c2 in zip(*indices_to_calc)
-            ]
+        pair_names = [
+            (comm_names[c1], comm_names[c2]) for c1, c2 in zip(*indices_to_calc)
+        ]
+        pairs_difference = [
+            self.vectors.iloc[c2] - self.vectors.iloc[c1]
+            for c1, c2 in zip(*indices_to_calc)
+        ]
 
-            self.pairs_cache = pd.DataFrame(
-                index=pd.MultiIndex.from_tuples(pair_names), data=pairs_difference
-            )
-        return self.pairs_cache
+        return pd.DataFrame(
+            index=pd.MultiIndex.from_tuples(pair_names), data=pairs_difference
+        )
 
     def generate_dimensions_from_seeds(
         self, seeds: Sequence[SeedPair]
@@ -86,18 +85,14 @@ class DimensionGenerator:
         Returns:
             List[Dimension]: List of augmented dimension representation.
         """
-        return [self.augment_seed(x) for x in seeds]
+        return [self.augment_seed_direction(x) for x in seeds]
 
-    def augment_seed(self, seed_pair: SeedPair) -> Dimension:
-        """Augment seed pair for a more robust representation of the dimension.
+    def augment_seed_direction(self, seed_pair: SeedPair) -> Dimension:
+        """Augment seed pair direction for a more robust representation of the dimension.
 
         All pairs are ranked based on the cosine similarity of their vector difference
         with the vector difference of the seed pair: cos(s2-s1, c2-c1). Additional pairs
-        are then selected greedily. The most similar pair to the original seed pair
-        that has no overlap in communities with the seed pair or any of the previously
-        selected pairs is selected, and this process is repeated until k - 1
-        additional pairs are selected, which results in the k pairs used to create
-        the dimension.
+        are then selected greedily.
 
         Args:
             seed_pair (SeedPair): Dimension seed pair.
@@ -105,34 +100,48 @@ class DimensionGenerator:
         Returns:
             Dimension: Augmented dimension representation.
         """
-        
-        # 2 x 1
-        seed_indices = np.array([seed_pair]).T
-        
-        # 1 x N
+                
+        # 1-D Vector
         seed_direction = (
-            self.vectors.loc[seed_indices[1]].values
-            - self.vectors.loc[seed_indices[0]].values
+            self.vectors.loc[seed_pair[1]].to_numpy()
+            - self.vectors.loc[seed_pair[0]].to_numpy()
         )
-
-        # N x 1. No me queda claro por que hace producto interno en vez de cosine similarity 
-        # los vectores no estan normalizados, asi que no son equivalentes 
-        seed_similarities = np.dot(
-            self.nearest_neighbours_pairs_diff, seed_direction.T
-        )
-
-        assert (seed_similarities >= -1).all()
-        assert (seed_similarities <= 1).all()
+        nn_directions = self.nearest_neighbours_directions()
         
-        # N x 1 Matrix to 1-d Vector
-        seed_similarities = seed_similarities.max(axis=1)
+        # 1-D Vector. No me queda claro por que hace producto interno en vez de cosine similarity 
+        # los vectores no estan normalizados, asi que no son equivalentes 
+        seed_similarities = np.dot(nn_directions, seed_direction.T)
 
-        directions = self.nearest_neighbours_pairs_diff.iloc[
+        # assert (seed_similarities >= -1).all()
+        # assert (seed_similarities <= 1).all()
+
+        directions = nn_directions.iloc[
             seed_similarities.argsort()[::-1] # Sort DESC nearest neighbours by similarity
         ]
 
-        # make directions unique subreddits (subreddit can only occur once)
+        directions = self.augmentation_algorithm(seed_pair, seed_direction, directions)
+        return np.sum(directions.to_numpy(), axis=0)
+
+    def augmentation_algorithm(self, seed_pair: SeedPair, seed_direction: np.array, directions: pd.DataFrame) -> pd.DataFrame:
+        """ Seed augmentation algorithm.
+        
+        The most similar pair to the original seed pair
+        that has no overlap in communities with the seed pair or any of the previously
+        selected pairs is selected, and this process is repeated until k - 1
+        additional pairs are selected, which results in the k pairs used to create
+        the dimension.
+
+        Args:
+            seed_pair (SeedPair): Dimension seed pair.
+            seed_direction (np.array): Seed pair direction.
+            directions (pd.DataFrame): Sorted nearest neighbours directions.
+
+        Returns:
+            pd.DataFrame: Augmented seed pair dimension directions.
+        """        
         ban_list = list(seed_pair)
+
+        # Este algoritmo hay que revisarlo.
         i = -1  # to filter out seed pair
         while i < len(directions) and i < self.k + 1:
             ban_list.extend(directions.index[i])
@@ -146,17 +155,12 @@ class DimensionGenerator:
 
             i += 1
 
-        # Add seeds to the top
         directions = pd.DataFrame(
             index=pd.MultiIndex.from_tuples([seed_pair] + directions.index.tolist()),
-            data=np.concatenate((seed_direction, directions.to_numpy())),
+            data=np.concatenate([[seed_direction], directions.to_numpy()]),
         )
-
         direction_group = directions.iloc[0:self.k]
-
-        dimension = np.sum(direction_group.values, axis=0)
-
-        return dimension
+        return directions
 
     def get_scores_from_seeds(self, seeds: List[SeedPair], names: List[str]):
         """Calculate dimensions scores.
@@ -173,7 +177,7 @@ class DimensionGenerator:
         dimensions = self.generate_dimensions_from_seeds(seeds)
         for name, dimension in zip(names, dimensions):
             columns[name] = np.dot(
-                self.vectors.values, dimension / np.linalg.norm(dimension)
+                self.vectors.to_numpy(), dimension / np.linalg.norm(dimension)
             )
 
         return pd.DataFrame(columns, index=self.vectors.index)
